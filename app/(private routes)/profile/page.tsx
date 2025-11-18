bbdd568651ff2bdf74cf4b16c2078e0fb793f2f8
Skaleivt <skaleiviktor@gmail.com>
2025-11-18 15:59:33 +0200

    fix profile

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { notFound } from 'next/navigation';
 import Container from '@/components/Container/Container';
 import css from './ProfilePage.module.css';
